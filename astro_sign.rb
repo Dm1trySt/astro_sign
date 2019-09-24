deecc79bb0094ea6dc236a8a0970a905e117702c
Dmitry <dimm137@mail.ru>
2019-09-24 16:00:17 +0300

    astro_sign

---
+++ astro_sign.rb
@@ -0,0 +1,27 @@
+require 'json'
+require 'date'
+
+file = File.read('signs.json', encoding: 'UTF-8')
+signs = JSON.parse(file)
+
+puts "Когда вы родились(укажите дау в форме ДД.ММ, например, 08.03)"
+input_date = STDIN.gets.chomp
+
+user_birth_date = Date.parse(input_date + '.2000')
+
+user_sign = nil
+
+signs.each do |key, sign|
+  dates = sign['dates'].split('..')
+
+  if
+  (Date.parse(dates[0]+ '.2000')<=user_birth_date)&&
+    (user_birth_date <= Date.parse(dates[1]+'.2000'))
+
+    user_sign = sign
+  end
+
+end
+
+puts user_sign['dates']
+puts user_sign['text']
